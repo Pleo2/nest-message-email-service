56cc85cac1941dfd22a01e7e11f259059b2483ca
pleo2 <leooel23m@gmail.com>
2025-10-05 21:04:02 -0400

    feat(app): add ThrottlerGuard as a global guard and restructure throttler configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { ThrottlerModule } from '@nestjs/throttler'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 
 import { HealthModule } from '@/app/health/health.module'
 import { LoggerModule } from '@/shared/logger/logger.module'
@@ -11,6 +11,7 @@ import configuration from '../config/configuration'
 import { databaseImports } from '../config/database/database.imports'
 import { OtpModule } from '@/src/otp'
 import { redisImports } from '../config/redis/redis.imports'
+import { APP_GUARD } from '@nestjs/core'
 
 const isTest = process.env.NODE_ENV === 'test'
 
@@ -27,10 +28,21 @@ const coreImports = [
 	}),
 
 	ThrottlerModule.forRoot([
-		{
-			ttl: 60_000,
-			limit: 100,
-		},
+			{
+				name: 'short',
+				ttl: 1000, // 1 segundo
+				limit: 3, // 3 requests
+			},
+			{
+				name: 'medium',
+				ttl: 10_000, // 10 segundos
+				limit: 20,
+			},
+			{
+				name: 'long',
+				ttl: 60_000, // 1 minuto
+				limit: 100,
+			},
 	]),
 	LoggerModule,
 	HealthModule,
@@ -41,5 +53,11 @@ const coreImports = [
 	imports: isTest
 		? coreImports
 		: [...coreImports, ...databaseImports, ...redisImports, OtpModule],
+    providers: [
+        {
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard, // ← Global guard
+		},
+    ],
 })
 export class AppModule {}
