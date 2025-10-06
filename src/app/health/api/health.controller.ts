af98741f0f71539919147e41c76a2f4c36a251f8
pleo2 <leooel23m@gmail.com>
2025-10-05 22:11:13 -0400

    feat(health): add SkipAppValidation decorator to health endpoint

--- src/app/health/api/health.controller.ts
+++ src/app/health/api/health.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, HttpCode, Inject, Logger } from '@nestjs/common'
 
+import { SkipAppValidation } from '@/src/decorators'
+
 @Controller('health')
 export class HealthController {
 	constructor(@Inject(Logger) private readonly logger: Logger) {}
 
 	@Get()
 	@HttpCode(200)
+	@SkipAppValidation()
 	run() {
 		this.logger.log('Health endpoint called!')
 		return { status: 'ok' }
